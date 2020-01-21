482109c20a24a95e73d2d7a446061095d8f8b992
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-21 03:09:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,8 @@ def third_challenge
       :hero => {
         :name "Romeo", :age "15", status: "alive"
         },
-      :hero_friends => [{}]
+      :hero_friends => [
+        {name: "Benvolio", :age "17", :attitude "worried"},{:name "Mercutio", :age "18", :attitude "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {
@@ -23,7 +24,9 @@ def third_challenge
       :heroine => {
         :name "Juliet", :age "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {:name "Steven", :age "30", :attitude "worried"}
+        ]
    }
   }
 
